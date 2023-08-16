0c221c58cdba357f35abcdfd8afad61745cc8f0b
Matias Tello <matiasfernandotello@gmail.com>
2023-08-15 23:37:20 -0300

    prim

---
+++ index.js
@@ -0,0 +1,18 @@
+
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+
+// Servir archivos estáticos
+app.use(express.static('public'))
+// Configurar rutas
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// poner a escuchar la app en un puerto
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
